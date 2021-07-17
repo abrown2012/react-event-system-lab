a846897ac57f1410ae304b714ea3ca22f7df6aa5
Agnieszka Brown <abrown2012@icloud.com>
2021-07-17 15:58:08 -0400

    commit tree e1dcdc048eab9de5967594b9ccff7515e37f2594

--- src/components/EyesOnMe.js
+++ src/components/EyesOnMe.js
@@ -1,1 +1,24 @@
+import React from 'react'
+
+class EyesOnMe extends React.Component {
+
+    handleFocus = () => {
+        console.log('Good!')
+    }
+
+    handleOffFocus = () => {
+        console.log('Hey! Eyes on me!')
+    }
+
+    render() {
+        return(
+            <button onFocus = {this.handleFocus} onBlur = {this.handleOffFocus}>
+
+            </button>
+        )
+    }
+    
+}
+
+export default EyesOnMe;
 // Code EyesOnMe Component Here
